994b24a94951a43d50c5db9816b3fff6524b40e0
Kevin <kevinadikasaputragithub@gmail.com>
2026-06-07 19:04:50 +0700

    :recycle: Update dependecies and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,20 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    files: ["**/*.test.ts", "**/*.test.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "coverage/**",
   ]),
 ]);
 
